7e39069057947ce6497d47f6615e2d88dba514bf
Andrew DalPino <andrewdalpino@icloud.com>
2025-12-10 12:51:49 -0600

    Revert "Add unit tests"
    This reverts commit 60e2718b70e925cf9f30ebd723ea098a66ecc0b9.

--- data.py
+++ data.py
@@ -9,22 +9,44 @@ from torch.nn.utils.rnn import pad_sequence
 
 from esm.tokenization import EsmSequenceTokenizer
 
+import networkx as nx
 
-class SwissProt(Dataset):
+from networkx import DiGraph
+
+
+class AmiGO(Dataset):
     """
     A collection of high-quality human-annotated protein sequences and their associated gene
     ontology terms taken from the SwissProt subsection of the UniProt database.
     """
 
-    DATASET_NAME = "andrewdalpino/SwissProt-Gene-Ontology"
+    DATASET_NAME = "andrewdalpino/AmiGO"
+
+    AVAILABLE_SUBSETS = {"all", "mf", "cc", "bp"}
+
+    AVAILABLE_SPLITS = {"train", "test"}
 
     def __init__(
         self,
+        subset: str,
+        split: str,
+        graph: DiGraph,
         tokenizer: EsmSequenceTokenizer,
         min_sequence_length: int = 1,
         max_sequence_length: int = 2048,
     ):
         super().__init__()
+
+        if subset not in self.AVAILABLE_SUBSETS:
+            raise ValueError(f"Subset '{subset}' is invalid.")
+
+        if split not in self.AVAILABLE_SPLITS:
+            raise ValueError(f"Split '{split}' is invalid.")
+
+        if not nx.is_directed_acyclic_graph(graph):
+            raise ValueError(
+                "Invalid GO graph, must be a directed acyclic graph (DAG)."
+            )
 
         if min_sequence_length < 1:
             raise ValueError(
@@ -36,9 +58,28 @@ class SwissProt(Dataset):
                 f"Max sequence length must be greater than 0, {max_sequence_length} given."
             )
 
-        dataset = load_dataset(self.DATASET_NAME)
+        dataset = load_dataset(self.DATASET_NAME, subset)
 
-        dataset = dataset["train"]
+        go_ids_to_label_indices = {}
+
+        label_index = 0
+
+        for subset in dataset.values():
+            for sample in subset:
+                for go_id in sample["go_terms"]:
+                    if go_id in go_ids_to_label_indices:
+                        continue
+
+                    if go_id not in graph:
+                        continue
+
+                    go_ids_to_label_indices[go_id] = label_index
+
+                    label_index += 1
+
+        num_classes = len(go_ids_to_label_indices)
+
+        dataset = dataset[split]
 
         dataset = dataset.filter(
             lambda sample: len(sample["sequence"]) >= min_sequence_length
@@ -46,16 +87,27 @@ class SwissProt(Dataset):
         )
 
         self.dataset = dataset
+        self.graph = graph
         self.tokenizer = tokenizer
         self.min_sequence_length = min_sequence_length
         self.max_sequence_length = max_sequence_length
+        self.go_ids_to_label_indices = go_ids_to_label_indices
+        self.num_classes = num_classes
+
+    @property
+    def label_indices_to_go_ids(self):
+        """
+        Returns a dictionary mapping label indices to their corresponding gene ontology terms.
+        """
+
+        return {index: go_id for go_id, index in self.go_ids_to_label_indices.items()}
 
     def collate_pad_right(self, batch):
         """
         Pads the sequences in the batch to the maximum sequence length on the right.
         """
 
-        sequences = [sequence for sequence in batch]
+        sequences = [sequence for sequence, _ in batch]
 
         padded_sequences = pad_sequence(
             sequences,
@@ -64,9 +116,11 @@ class SwissProt(Dataset):
             padding_side="right",
         )
 
-        return padded_sequences
+        labels = torch.stack([label for _, label in batch])
 
-    def __getitem__(self, index: int) -> Tensor:
+        return padded_sequences, labels
+
+    def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
         sample = self.dataset[index]
 
         out = self.tokenizer(
@@ -77,11 +131,27 @@ class SwissProt(Dataset):
 
         tokens = out["input_ids"]
 
+        labels = [0.0] * self.num_classes
+
+        for go_id in sample["go_terms"]:
+            if go_id in self.go_ids_to_label_indices:
+                label_index = self.go_ids_to_label_indices[go_id]
+
+                labels[label_index] = 1.0
+
         x = torch.tensor(tokens, dtype=torch.int64)
+        y = torch.tensor(labels, dtype=torch.float32)
 
         assert x.size(0) <= self.max_sequence_length
+        assert y.size(0) == self.num_classes
 
-        return x
+        return x, y
 
     def __len__(self):
         return len(self.dataset)
+
+
+class AmiGOBoost(AmiGO):
+    """The AmiGO dataset with additional phylogenetically-inferred annotations."""
+
+    DATASET_NAME = "andrewdalpino/AmiGO-Boost"
